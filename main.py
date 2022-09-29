e4d75687b39b0afcb9971f9f7584c53370714f0f
Mauricio Cruz <bmauricio.cruz07@gmail.com>
2022-09-29 11:02:13 -0500

    Nomenclaturas

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+nomenclaturas = {
+    "CARRERA" : ["CARRERA","KRA","CRA", "CR", "KR"],
+    "CALLE" : ["CALLE","CLL","CL"],
+    "APARTAMENTO" : ["APTO","APT","APARTAMENTO"],
+    "AVENIDA" : ["AVENIDA","AV","AVE"],
+    "AUTOPISTA" : ["AUTOPISTA","AUT"],
+    "BARRIO" : ["BARRIO","BRR"],
+    "DIAGONAL" : ["DIAGONAL","DIA","DG"],
+    "EDIFICIO" : ["EDIFICIO","ED"],
+    "TRANSVERSAL" : ["TRANSVERSAL","TV"],
+    "ADMINISTRACIÓN" : ["ADMINISTRACIÓN","AD"]
+}
 
+
+print(nomenclaturas)
+
+'''
+1)Terminar nomenclaturas
+2)Preguntarle a Marco sobre el formato
+3)Separar por espacios y definir que son nomenclaturas/números/palabras/carácteresEspeciales.
+4)
+'''
