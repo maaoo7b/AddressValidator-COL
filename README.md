2a53379fe671a62180b0a39a85ef53c1376b449e
Mauricio Cruz <bmauricio.cruz07@gmail.com>
2022-10-28 22:37:14 -0500

    Refactoring "temporal" as addressValidator. Ordering code into functions. Added README.md file.

---
+++ README.md
@@ -0,0 +1,36 @@
+-----
+**addressValidator_COL**
+-
+-----
+Brings easyness to evaluate
+urban as well as rural addresses from Colombia.
+
+It works by R.E containing all possibilities of addresses and its corresponding abbreviation.
+
+
+
+-----
+How does it works?
+- 
+- All you've gotta do it's upload a .txt file and let magic do it's work.
+-----
+Keep in mind:
+- 
+- Addresses words must begin with its first letter capitalized.
+- Other words complementaring the address must be capitalized
+- Doesn't matter if there are any whitespaces " " in the line
+-----
+Example:
+-
+**Accepted addresses:**
+- Kra 10A # 7A-29
+- Corregimiento DEL TRES Via SAN PEDRO DE URABA TURBO
+- Calle 1A 13A 13 Ap 201
+- Municipio DE CUITIVA Vereda LLANO DE ALARCON
+- Carrera 14 No 127-11 Consultorio 304
+
+**Non accepted addresses:**
+- KRA 10A # 7A-29
+- CORREGIMIENTODELTRESVIASANPEDRODEURABATURBO
+- Municipio de Cuitiva Vereda llano de alarcon
+
--- addresValidator.py
+++ addresValidator.py
@@ -1,5 +1,5 @@
 import re
-regexNomenclaturasA = "Administracion|Ad|Admin|Agencia|Agrupacion|Agp|Almacen|Alm|Altillo|Al|Apartado|Aptdo|Apto|Apt|Apartamento|Autopista|Aut|Avenida|Av|Ave"
+regexNomenclaturasA = "Administracion|Ad|Admin|Agencia|Agrupacion|Agp|Almacen|Alm|Altillo|Al|Apartado|Aptdo|Apto|Apt|Ap|Apartamento|Autopista|Aut|Avenida|Av|Ave"
 regexNomenclaturasB = "Barrio|Brr|Bloque|Bl|Blq|Bodega|Bg|Boulevar|Blv"
 regexNomenclaturasC = "Calle|Cll|Cl|Camino|Cn|Carrera|Cra|Cr|Kr|Kra|Carretera|Carr|Casa|Ca|Centro Comercial|Cc|Circunvalar|Crv|Ciudadela|Cd|Conjunto|Conj|Conjunto Residencial|Con|Consultorio|Cons|Corregimiento|Corr"
 regexNomenclaturasD = "Departamento|Depto|Dpto|Deposito|Dp|Deposito Sotano|Ds|Diagonal|Dia|Dg"
@@ -9,7 +9,7 @@ regexNomenclaturasG = "Garaje|Ga|Garaje Sotano|Gs"
 regexNomenclaturasH = "Hacienda|Hc"
 regexNomenclaturasI = "Interior|In"
 regexNomenclaturasK = "Kilometro|Km"
-regexNomenclaturasL = "Local|Lc|Lote|Lt"
+regexNomenclaturasL = "Local|Lc|Lote|Lt|Lugar"
 regexNomenclaturasM = "Manzana|Mz|Modulo|Md|Municipio|Mcp|Mcpio|Mnpio"
 regexNomenclaturasN = "NORTE"
 regexNomenclaturasO = "OCCIDENTE|OESTE|Oficina|Of|ORIENTE"
@@ -17,33 +17,22 @@ regexNomenclaturasP = "Parcela|Pa|Parque|Par|Parqueadero|Pq|Pasaje|Pj|Paseo|Ps|P
 regexNomenclaturasS = "Salon|Sa|Salon Comunal|Sc|Sector|Sec|Semisotano|Ss|Solar|Sl|Sotano|St|Suite|Su|SUR|Super Manzana|SM"
 regexNomenclaturasT = "Terminal|Ter|Terraza|Tz|Torre|To|Transversal|Transv|Tv"
 regexNomenclaturasU = "Unidad|Un|Unidad Residencial|Ur|Urbanizacion|Urb"
-regexNomenclaturasV = "Variante|Vte|Vereda|Vda"
+regexNomenclaturasV = "Variante|Vte|Vereda|Vda|Via"
 regexNomenclaturasZ = "Zona|Zn"
 
-
-archivo = open("words.txt")
-palabras=archivo.read()
-
-# coding=utf8
-# the above tag defines encoding for this document and is for Python 2.x compatibility
-
-
 cadenaNomenclaturas = regexNomenclaturasA+"|"+regexNomenclaturasB+"|"+regexNomenclaturasC+"|"+regexNomenclaturasD+"|"+regexNomenclaturasE+"|"+regexNomenclaturasF+"|"+regexNomenclaturasG+"|"+regexNomenclaturasH+"|"+regexNomenclaturasI+"|"+regexNomenclaturasK+"|"+regexNomenclaturasL+"|"+regexNomenclaturasM+"|"+regexNomenclaturasN+"|"+regexNomenclaturasO+"|"+regexNomenclaturasP+"|"+regexNomenclaturasS+"|"+regexNomenclaturasT+"|"+regexNomenclaturasU+"|"+regexNomenclaturasV+"|"+regexNomenclaturasZ
-print(cadenaNomenclaturas)
-cadenaNomenclaturasMayuscula= cadenaNomenclaturas.upper()
 regex = r"^(("+cadenaNomenclaturas+")){1} ?([0-9A-Z ]+)? ?(#|-|No)?([0-9A-Z ]+) ?(("+cadenaNomenclaturas+")|(#|No|-))?([ A-Z0-9]+)? ?(("+cadenaNomenclaturas+")|(#|No|-| ?)) ?([ 0-9A-Z])+(-|\/){0,}[A-Z0-9]+$"
 
+def readAddresses(fileAdresses):
+    file = open(fileAdresses)
+    addresses = file.read()
+    matches = re.finditer(regex, addresses, re.MULTILINE)
+    validAddresses(matches)
 
 
-test_str = (palabras)
-
-
-matches = re.finditer(regex, test_str, re.MULTILINE)
-
-for matchNum, match in enumerate(matches, start=1):
-
-    print("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum=matchNum, start=match.start(),
-                                                                        end=match.end(), match=match.group()))
-
-archivo.close()
-# Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
+def validAddresses(matches):
+    file = open('validAdresses.txt', 'w', encoding='utf-8')
+    for matchNum, match in enumerate(matches, start=1):
+        print("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum=matchNum, start=match.start(),end=match.end(), match=match.group()))
+        file.write('Nomenclatura/Dirección: ' + match.group() + ' ----> VÁLIDA\n')
+    file.close()
